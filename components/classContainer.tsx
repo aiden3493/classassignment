4b4532a0831639fded6e3d722a3f6a6649d4f17f
AIDEN <76594690+aiden3493@users.noreply.github.com>
2022-02-28 14:11:43 +0900

    first-commit

---
+++ components/classContainer.tsx
@@ -0,0 +1,54 @@
+import styles from "../styles/Home.module.css";
+import {motion} from "framer-motion";
+import {useState} from "react";
+
+export default function ClassContainer(props:any) {
+    const [isOpen, setIsOpen] = useState(false)
+    const studentArray = `${props.student}`.split(" ")
+    const half1 = studentArray.slice(undefined, Math.ceil(studentArray.length / 2))
+    const half2 = studentArray.slice(Math.ceil(studentArray.length / 2))
+    return isOpen ? (
+        <motion.div className={styles.openedClassContainer}
+                    onClick={() => setIsOpen(!isOpen)}
+                    layout
+                    layoutId={`${props.classNum}Container`}
+                    transition={{duration: 0.2}}
+                    style={{height: "300px"}}>
+            <motion.h2 layout layoutId={`${props.classNum}P`} style={{marginTop: "15px"}}>{props.classNum}반</motion.h2>
+            <motion.div className={styles.studentContainer}>
+                <motion.ul layout
+                           className={styles.studentList}
+                           initial={{ opacity: 0 }}
+                           animate={{ opacity: 1 }}
+                           exit={{ opacity: 0 }}
+                           transition={{ duration: 0.5 }}>
+                        {half1.map((name) =>
+                            <li><h3>{name}</h3></li>
+                        )}
+
+                </motion.ul>
+                <motion.ul layout
+                           className={styles.studentList2}
+                           initial={{ opacity: 0 }}
+                           animate={{ opacity: 1 }}
+                           exit={{ opacity: 0 }}
+                           transition={{ duration: 0.5 }}>
+                    {half2.map((name) =>
+                        <li><h3>{name}</h3></li>
+                    )}
+                </motion.ul>
+            </motion.div>
+        </motion.div>
+    ) : (
+        <motion.div className={styles.classContainer}
+                    onClick={() => setIsOpen(!isOpen)}
+                    layout
+                    layoutId={`${props.classNum}Container`}
+                    transition={{duration: 0.2}}
+                    style={{height: "60px"}}>
+            <motion.h2 layout layoutId={`${props.classNum}P`}>{props.classNum}반</motion.h2>
+        </motion.div>
+
+    )
+
+}
